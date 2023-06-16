5add2c7e5627788f18d343e1fd746c14f4559b9b
Anwar Hidayat <15167551+irainia@users.noreply.github.com>
2023-06-16 17:54:35 +0700

    feat: add flatten upstream function

--- task/bq2bq/upstream/upstream.go
+++ task/bq2bq/upstream/upstream.go
@@ -4,3 +4,15 @@ type Upstream struct {
 	Resource  Resource
 	Upstreams []*Upstream
 }
+
+func FlattenUpstreams(upstreams []*Upstream) []Resource {
+	var output []Resource
+	for _, u := range upstreams {
+		output = append(output, u.Resource)
+
+		nested := FlattenUpstreams(u.Upstreams)
+		output = append(output, nested...)
+	}
+
+	return output
+}
