f778337d5168d67e6b78f42f39c08385ca08843b
Dery Rahman Ahaddienata <dery.ra@gmail.com>
2024-09-30 17:07:58 +0700

    feat: add replace load method capability
    Signed-off-by: Dery Rahman Ahaddienata <dery.ahaddienata@go-jek.com>

--- max2max/internal/loader/replace.go
+++ max2max/internal/loader/replace.go
@@ -1,23 +1,25 @@
 package loader
 
 import (
+	"fmt"
 	"log/slog"
+	"strings"
 )
 
 type replaceLoader struct {
 	logger *slog.Logger
 }
 
-func NewReplaceLoader(logger *slog.Logger) *replaceLoader {
+func NewReplaceLoader(logger *slog.Logger) (*replaceLoader, error) {
 	return &replaceLoader{
 		logger: logger,
-	}
+	}, nil
 }
 
 func (l *replaceLoader) GetQuery(tableID, query string) string {
-	return "-- TODO replace loader"
+	return fmt.Sprintf("INSERT OVERWRITE TABLE %s %s", tableID, query)
 }
 
-func (l *replaceLoader) GetPartitionedQuery(tableID, query string, partitionName []string) string {
-	return "-- TODO replace loader"
+func (l *replaceLoader) GetPartitionedQuery(tableID, query string, partitionNames []string) string {
+	return fmt.Sprintf("INSERT OVERWRITE TABLE %s PARTITION (%s) %s", tableID, strings.Join(partitionNames, ", "), query)
 }
