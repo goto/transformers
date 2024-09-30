f3221ba2dbda837175ebb9b4ae08a899ace83a84
Dery Rahman Ahaddienata <dery.ra@gmail.com>
2024-09-30 16:10:01 +0700

    refactor: clean up unecessary metrics

--- max2max/internal/loader/append.go
+++ max2max/internal/loader/append.go
@@ -1,48 +1,25 @@
 package loader
 
 import (
-	"context"
 	"fmt"
 	"log/slog"
 	"strings"
-
-	"go.opentelemetry.io/otel"
-	"go.opentelemetry.io/otel/attribute"
-	"go.opentelemetry.io/otel/metric"
 )
 
 type appendLoader struct {
-	logger       *slog.Logger
-	meter        metric.Meter
-	queryCounter metric.Int64Counter
+	logger *slog.Logger
 }
 
 func NewAppendLoader(logger *slog.Logger) (*appendLoader, error) {
-	meter := otel.Meter("loader")
-	queryCounter, err := meter.Int64Counter("query.count")
-	if err != nil {
-		return nil, err
-	}
-
 	return &appendLoader{
-		logger:       logger,
-		meter:        meter,
-		queryCounter: queryCounter,
+		logger: logger,
 	}, nil
 }
 
 func (l *appendLoader) GetQuery(tableID, query string) string {
-	l.queryCounter.Add(context.Background(), 1, metric.WithAttributes(
-		attribute.String("method", "GetQuery"),
-		attribute.String("tableID", tableID),
-	))
 	return fmt.Sprintf("INSERT INTO TABLE %s %s", tableID, query)
 }
 
 func (l *appendLoader) GetPartitionedQuery(tableID, query string, partitionNames []string) string {
-	l.queryCounter.Add(context.Background(), 1, metric.WithAttributes(
-		attribute.String("method", "GetPartitionedQuery"),
-		attribute.String("tableID", tableID),
-	))
 	return fmt.Sprintf("INSERT INTO TABLE %s PARTITION (%s) %s", tableID, strings.Join(partitionNames, ", "), query)
 }
