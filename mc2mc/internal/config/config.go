93d3e70824b3eaf1748b7caa9821b11a00f36aa0
Dery Rahman Ahaddienata <dery.ra@gmail.com>
2024-11-07 13:51:38 +0700

    refactor: rename MAXCOMPUTE_ config to MC_

--- mc2mc/internal/config/config.go
+++ mc2mc/internal/config/config.go
@@ -48,8 +48,8 @@ func NewConfig() (*Config, error) {
 	cfg.Config.AccessKey = cred.AccessKey
 	cfg.Config.Endpoint = cred.Endpoint
 	cfg.Config.ProjectName = cred.ProjectName
-	cfg.Config.HttpTimeout = getEnvDuration("MAXCOMPUTE_HTTP_TIMEOUT", "10s")
-	cfg.Config.TcpConnectionTimeout = getEnvDuration("MAXCOMPUTE_TCP_TIMEOUT", "30s")
+	cfg.Config.HttpTimeout = getEnvDuration("MC_HTTP_TIMEOUT", "10s")
+	cfg.Config.TcpConnectionTimeout = getEnvDuration("MC_TCP_TIMEOUT", "30s")
 
 	return cfg, nil
 }
