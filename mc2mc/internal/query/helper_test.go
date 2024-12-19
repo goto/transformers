0fe3ecec73fc45586744ba3a6d15f5c6b01ac815
Dery Rahman Ahaddienata <dery.ra@gmail.com>
2024-12-20 06:39:24 +0700

    fix: query separator contains semicolon in query

--- mc2mc/internal/query/helper_test.go
+++ mc2mc/internal/query/helper_test.go
@@ -1,7 +1,6 @@
 package query_test
 
 import (
-	"strings"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -22,14 +21,14 @@ select * from playground`
 
 		header, query := query.SeparateHeadersAndQuery(q1)
 		assert.Empty(t, header)
-		assert.Contains(t, query, q1)
+		assert.Equal(t, "select * from playground", query)
 	})
 	t.Run("splits headers and query", func(t *testing.T) {
 		q1 := `set odps.sql.allow.fullscan=true;
 select * from playground`
 		headers, query := query.SeparateHeadersAndQuery(q1)
 		assert.Equal(t, "set odps.sql.allow.fullscan=true;", headers)
-		assert.Equal(t, "select * from playground", strings.TrimSpace(query))
+		assert.Equal(t, "select * from playground", query)
 	})
 	t.Run("works with query of multiple headers", func(t *testing.T) {
 		q1 := `set odps.sql.allow.fullscan=true;
@@ -53,6 +52,16 @@ set odps.sql.python.version=cp37;`
 from presentation.main.important_date
 where CAST(event_timestamp as DATE) = '{{ .DSTART | Date }}'
   and client_id in ('123')`
-		assert.Contains(t, query, expectedQuery)
+		assert.Contains(t, expectedQuery, query)
+	})
+	t.Run("works with query contains semicolon", func(t *testing.T) {
+		q1 := `set odps.sql.allow.fullscan=true;
+select CONCAT_WS('; ', COLLECT_LIST(dates)) AS dates from presentation.main.important_date`
+		headers, query := query.SeparateHeadersAndQuery(q1)
+		expectedHeader := `set odps.sql.allow.fullscan=true;`
+		assert.Equal(t, expectedHeader, headers)
+
+		expectedQuery := `select CONCAT_WS('; ', COLLECT_LIST(dates)) AS dates from presentation.main.important_date`
+		assert.Equal(t, expectedQuery, query)
 	})
 }
