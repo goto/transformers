54e753e40ea42a4d325c09eeca928443be41d686
Dery Rahman Ahaddienata <dery.ra@gmail.com>
2025-01-06 16:04:33 +0700

    fix(mc2mc): get table info for non default schema (#58)
    fix: get table info for non default schema

--- mc2mc/internal/client/odps.go
+++ mc2mc/internal/client/odps.go
@@ -57,13 +57,8 @@ func (c *odpsClient) ExecSQL(ctx context.Context, query string) error {
 // GetPartitionNames returns the partition names of the given table
 // by querying the table schema.
 func (c *odpsClient) GetPartitionNames(_ context.Context, tableID string) ([]string, error) {
-	splittedTableID := strings.Split(tableID, ".")
-	if len(splittedTableID) != 3 {
-		return nil, errors.Errorf("invalid tableID (tableID should be in format project.schema.table): %s", tableID)
-	}
-	project, schema, name := splittedTableID[0], splittedTableID[1], splittedTableID[2]
-	table := odps.NewTable(c.client, project, schema, name)
-	if err := table.Load(); err != nil {
+	table, err := getTable(c.client, tableID)
+	if err != nil {
 		return nil, errors.WithStack(err)
 	}
 	var partitionNames []string
@@ -77,13 +72,8 @@ func (c *odpsClient) GetPartitionNames(_ context.Context, tableID string) ([]str
 // GetOrderedColumns returns the ordered column names of the given table
 // by querying the table schema.
 func (c *odpsClient) GetOrderedColumns(tableID string) ([]string, error) {
-	splittedTableID := strings.Split(tableID, ".")
-	if len(splittedTableID) != 3 {
-		return nil, errors.Errorf("invalid tableID (tableID should be in format project.schema.table): %s", tableID)
-	}
-	project, schema, name := splittedTableID[0], splittedTableID[1], splittedTableID[2]
-	table := odps.NewTable(c.client, project, schema, name)
-	if err := table.Load(); err != nil {
+	table, err := getTable(c.client, tableID)
+	if err != nil {
 		return nil, errors.WithStack(err)
 	}
 	var columnNames []string
@@ -114,4 +104,33 @@ func addHints(query string) map[string]string {
 	}
 
 	return nil
+}
+
+// getTable returns the table with the given tableID
+func getTable(client *odps.Odps, tableID string) (*odps.Table, error) {
+	// save current project and schema
+	currProject := client.DefaultProjectName()
+	currSchema := client.CurrentSchemaName()
+	defer func() {
+		// restore current project and schema
+		client.SetDefaultProjectName(currProject)
+		client.SetCurrentSchemaName(currSchema)
+	}()
+
+	splittedTableID := strings.Split(tableID, ".")
+	if len(splittedTableID) != 3 {
+		return nil, errors.Errorf("invalid tableID (tableID should be in format project.schema.table): %s", tableID)
+	}
+	project, schema, name := splittedTableID[0], splittedTableID[1], splittedTableID[2]
+
+	// set project and schema to the table
+	client.SetDefaultProjectName(project)
+	client.SetCurrentSchemaName(schema)
+
+	// get table
+	table := client.Tables().Get(name)
+	if err := table.Load(); err != nil {
+		return nil, errors.WithStack(err)
+	}
+	return table, nil
 }
